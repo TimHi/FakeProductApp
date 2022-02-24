baeabdb917495a9a5e08e1f9129269a4d8185e16
Tim Hiller <TimHiller@live.de>
2022-02-25 00:20:21 +0100

    adding feldberg

---
+++ app/src/main/java/com/example/fakeproductapp/feature_product_list/presentation/ProductDetailViewFragment.kt
@@ -0,0 +1,10 @@
+package com.example.fakeproductapp.feature_product_list.presentation
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.fakeproductapp.feature_product_list.presentation.viewmodel.MainViewModel
+
+class ProductDetailViewFragment: Fragment() {
+    private val viewModel: MainViewModel by viewModels()
+
+}
--- app/src/main/java/com/example/fakeproductapp/feature_product_list/presentation/ProductListFragment.kt
+++ app/src/main/java/com/example/fakeproductapp/feature_product_list/presentation/ProductListFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import com.example.fakeproductapp.R
 import com.example.fakeproductapp.databinding.ProductListBinding
 import com.example.fakeproductapp.feature_product_list.presentation.adapter.ProductListAdapter
 import com.example.fakeproductapp.feature_product_list.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class FragmentCharacterList : Fragment() {
@@ -26,9 +29,8 @@ class FragmentCharacterList : Fragment() {
 
         val adapter = ProductListAdapter(ProductListAdapter.OnClickListener
         { product ->
-
             Toast.makeText(this.context, product.title, Toast.LENGTH_SHORT).show()
-
+            //Navigation.findNavController(requireView()).navigate(R.id.product_detail)
         })
 
         binding.productList.adapter = adapter
